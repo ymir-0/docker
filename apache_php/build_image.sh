17d018706adf1d48aa461152d40706a35f037801
ymir <mazieres.pierre@gmail.com>
2017-10-22 03:34:01 +0200

    edit apache/PHP

--- apache_php/build_image.sh
+++ apache_php/build_image.sh
@@ -1,3 +1,3 @@
-docker build -t apache_php_perso .
-docker run --name apache_php_perso -d -v ~/docker/apache_php/html:/var/www/html -p 80:80 apache_php_perso /bin/sh -c "service apache2 start ; tail -f /dev/null"
-
+IMAGE_CONTAINER_NAME="apache_php_perso"
+docker build -t ${IMAGE_CONTAINER_NAME} .
+docker run --name ${IMAGE_CONTAINER_NAME} -d -v ~/docker/apache_php/html:/var/www/html -p 80:80 ${IMAGE_CONTAINER_NAME} /bin/sh -c "service apache2 start ; tail -f /dev/null"
