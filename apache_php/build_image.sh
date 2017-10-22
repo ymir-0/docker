da03213c916b3b12d5f8f0f05ae0467ab4de31c0
ymir <mazieres.pierre@gmail.com>
2017-10-22 14:50:15 +0200

    edit apache/PHP

--- apache_php/build_image.sh
+++ apache_php/build_image.sh
@@ -1,3 +1,11 @@
 IMAGE_CONTAINER_NAME="apache_php_perso"
+HOST_SCRIPT_ABSOLUTE_PATH=`dirname $( readlink -f "$0" )` # absolute path is mandatory for volumes binding between host and container
 docker build -t ${IMAGE_CONTAINER_NAME} .
-docker run --name ${IMAGE_CONTAINER_NAME} -d -v html:/var/www/html -v apache2_conf:/etc/apache2  -p 80:80 ${IMAGE_CONTAINER_NAME} /bin/sh -c "service apache2 start ; tail -f /dev/null"
+docker run \
+    -d \
+    --name ${IMAGE_CONTAINER_NAME} \
+    -v ${HOST_SCRIPT_ABSOLUTE_PATH}/html:/var/www/html \
+    -v ${HOST_SCRIPT_ABSOLUTE_PATH}/apache2_conf:/etc/apache2 \
+    -v ${HOST_SCRIPT_ABSOLUTE_PATH}/apache2_log:/var/log/apache2 \
+    -p 80:80 ${IMAGE_CONTAINER_NAME} \
+    /bin/sh -c "service apache2 start ; tail -f /dev/null"
