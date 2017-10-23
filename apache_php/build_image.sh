1ed52e993310d0ffc20a59f5e6b74acf67b2ee7d
ymir <mazieres.pierre@gmail.com>
2017-10-23 22:27:50 +0200

    edit apache/PHP

--- apache_php/build_image.sh
+++ apache_php/build_image.sh
@@ -2,14 +2,16 @@ IMAGE_NAME="perso/apache_php"
 CONTAINER_NAME="perso_apache_php"
 HOST_SCRIPT_ABSOLUTE_PATH=`dirname $( readlink -f "$0" )` # absolute path is mandatory for volumes binding between host and container
 docker build -t ${IMAGE_NAME} .
-# shared volumes are not provided because they can :
-# - depends of your server configuration
-# - contains confidential data
-# you can get them from another instance
+# sample files can be found in archive : sample.tar.gz
+# they contains folders :
+# - apache2_conf : default apache configuration
+# - html : default index and test site
+# apache2_log : log file ; will be created if not existing
 docker run \
     -d \
     --name ${CONTAINER_NAME} \
     -v ${HOST_SCRIPT_ABSOLUTE_PATH}/html:/var/www/html \
     -v ${HOST_SCRIPT_ABSOLUTE_PATH}/apache2_conf:/etc/apache2 \
     -v ${HOST_SCRIPT_ABSOLUTE_PATH}/apache2_log:/var/log/apache2 \
-    -p 80:80 ${IMAGE_NAME}
+    -p 80:80 \
+    ${IMAGE_NAME}
