64eca86b4cd35bea187b4bed3c66c1a5da9ba9ec
ymir <mazieres.pierre@gmail.com>
2017-10-29 01:54:12 +0200

    edit mysql

--- mysql/build.sh
+++ mysql/build.sh
@@ -9,22 +9,24 @@ cp Dockerfile Dockerfile.pattern
 sed -i s/"{ROOT_PASSWORD}"/${ROOT_PASSWORD}/g Dockerfile
 sed -i s/"{USER_LOGIN}"/${USER_LOGIN}/g Dockerfile
 sed -i s/"{USER_PASSWORD}"/${USER_PASSWORD}/g Dockerfile
+# allow to edit shared volumes
+sudo chown -R ${USER} {log,data}
 # create image
 docker build -t ${IMAGE} .
 # restore Dockerfile
 mv Dockerfile.pattern Dockerfile
 # sample files can be found in archive : sample.tar.gz
 # they contains folders :
-# - apache2_conf : default apache configuration
-# - html : default index and test site
-# apache2_log : log file ; will be created if not existing
+# - configuration : default configuration
+# - data : default databases
+# log : will be created if not existing
 docker create \
     --name ${CONTAINER} \
     -v ${HOST_SCRIPT_ABSOLUTE_PATH}/configuration:/etc/mysql \
+    -v ${HOST_SCRIPT_ABSOLUTE_PATH}/data:/var/lib/mysql \
     -v ${HOST_SCRIPT_ABSOLUTE_PATH}/log:/var/log/mysql \
     -p 3306:3306 \
     ${IMAGE}
-#    -v ${HOST_SCRIPT_ABSOLUTE_PATH}/log:/var/log/mysql \
 # set fine container directory rights
 docker start ${CONTAINER}
 docker exec -it ${CONTAINER} /bin/bash -c "chown -R mysql:adm /var/log/mysql && chown -R mysql:mysql /var/lib/mysql"
