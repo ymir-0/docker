26052975644fea1a9e3dfca76abf9fce5e7473e4
ymir <mazieres.pierre@gmail.com>
2017-10-29 02:34:14 +0200

    edit apache/PHP

--- apache_php/build.sh
+++ apache_php/build.sh
@@ -3,13 +3,13 @@ HOST_SCRIPT_ABSOLUTE_PATH=`dirname $( readlink -f "$0" )` # absolute path is man
 docker build -t ${IMAGE} .
 # sample files can be found in archive : sample.tar.gz
 # they contains folders :
-# - apache2_conf : default apache configuration
+# - configuration : default configuration
 # - html : default index and test site
-# apache2_log : log file ; will be created if not existing
+# log : will be created if not existing
 docker create \
     --name ${CONTAINER} \
     -v ${HOST_SCRIPT_ABSOLUTE_PATH}/html:/var/www/html \
-    -v ${HOST_SCRIPT_ABSOLUTE_PATH}/apache2_conf:/etc/apache2 \
-    -v ${HOST_SCRIPT_ABSOLUTE_PATH}/apache2_log:/var/log/apache2 \
+    -v ${HOST_SCRIPT_ABSOLUTE_PATH}/configuration:/etc/apache2 \
+    -v ${HOST_SCRIPT_ABSOLUTE_PATH}/log:/var/log/apache2 \
     -p 80:80 \
     ${IMAGE}
