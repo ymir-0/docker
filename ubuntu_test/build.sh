95a5f727e04e67389d80b874f77ce1401832a109
ymir <mazieres.pierre@gmail.com>
2017-11-02 20:17:29 +0100

    edit ubuntu test

--- ubuntu_test/build.sh
+++ ubuntu_test/build.sh
@@ -1,7 +1,7 @@
 . configuration.sh
 HOST_SCRIPT_ABSOLUTE_PATH=`dirname $( readlink -f "$0" )` # absolute path is mandatory for volumes binding between host and container
-docker build -t ${IMAGE} .
 mkdir shared
+docker build -t ${IMAGE} .
 docker create \
     --name ${CONTAINER} \
     -v ${HOST_SCRIPT_ABSOLUTE_PATH}/shared:/home/shared \
