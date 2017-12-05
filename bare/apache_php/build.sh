9ae5d372be4d418fbffa93b7d8516bb69c2b7ed8
ymir <geantdegivre>
2017-12-05 09:10:07 +0100

    separate build vs. create & explicit test

--- bare/apache_php/build.sh
+++ bare/apache_php/build.sh
@@ -1,15 +1,3 @@
 . configuration.sh
 HOST_SCRIPT_ABSOLUTE_PATH=`dirname $( readlink -f "$0" )` # absolute path is mandatory for volumes binding between host and container
 docker build -t ${IMAGE} .
-# sample files can be found in archive : sample.tar.gz
-# they contains folders :
-# - configuration : default configuration
-# - html : default index and test site
-# log : will be created if not existing
-docker create \
-    --name ${CONTAINER} \
-    -v ${HOST_SCRIPT_ABSOLUTE_PATH}/html:/var/www/html \
-    -v ${HOST_SCRIPT_ABSOLUTE_PATH}/configuration:/etc/apache2 \
-    -v ${HOST_SCRIPT_ABSOLUTE_PATH}/log:/var/log/apache2 \
-    -p 80:80 \
-    ${IMAGE}
--- bare/apache_php/delete.sh
+++
@@ -1,4 +0,0 @@
-. configuration.sh
-docker stop ${CONTAINER}
-docker rm ${CONTAINER}
-docker rmi ${IMAGE}
