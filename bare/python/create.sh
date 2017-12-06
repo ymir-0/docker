4b674dd74409453447f95348bd29a71e350317e5
ymir <geantdegivre>
2017-12-06 20:13:52 +0100

    add sample

--- bare/python/create.sh
+++ bare/python/create.sh
@@ -1,5 +1,8 @@
 . configuration.sh
 HOST_SCRIPT_ABSOLUTE_PATH=`dirname $( readlink -f "$0" )` # absolute path is mandatory for volumes binding between host and container
+# sample files can be found in archive : sample.tar.gz
+# they contains folders :
+# - configuration : default configuration
 docker create \
     --name ${CONTAINER} \
     -v ${HOST_SCRIPT_ABSOLUTE_PATH}/configuration:/root/.config/pip \
