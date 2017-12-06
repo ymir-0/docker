6c2bc67566198ec5b5866118691e44e5f4712b56
ymir <geantdegivre>
2017-12-06 16:39:23 +0100

    export configuration folder

--- bare/python/create.sh
+++ bare/python/create.sh
@@ -2,4 +2,5 @@
 HOST_SCRIPT_ABSOLUTE_PATH=`dirname $( readlink -f "$0" )` # absolute path is mandatory for volumes binding between host and container
 docker create \
     --name ${CONTAINER} \
+    -v ${HOST_SCRIPT_ABSOLUTE_PATH}/configuration:/root/.config/pip \
     ${IMAGE}
