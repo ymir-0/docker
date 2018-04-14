cae84214ba264317f7a0ecec3a39292d8e86c46b
ymir <geantdegivre>
2018-04-14 19:58:01 +0200

    map conf/data/log

--- bare/postgresql/create.sh
+++ bare/postgresql/create.sh
@@ -7,6 +7,12 @@ HOST_SCRIPT_ABSOLUTE_PATH=`dirname $( readlink -f "$0" )` # absolute path is man
 # log : will be created if not existing
 docker create \
     --name ${CONTAINER} \
+    -v ${HOST_SCRIPT_ABSOLUTE_PATH}/configuration:/etc/postgresql \
+    -v ${HOST_SCRIPT_ABSOLUTE_PATH}/data:/var/lib/postgresql \
+    -v ${HOST_SCRIPT_ABSOLUTE_PATH}/log:/var/log/postgresql \
     -p 5432:5432 \
     ${IMAGE}
 # set fine container directory rights
+docker start ${CONTAINER}
+docker exec -it ${CONTAINER} /bin/bash -c "chown -R root:postgres /etc/postgresql ; chown -R root:postgres /var/log/postgresql ; chown -R postgres:postgres /var/lib/postgresql"
+docker stop ${CONTAINER}
